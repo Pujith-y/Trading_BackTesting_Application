5083739c0155eaddd7096ab58a422b012ed4bfe8
Pujith <pujith14l06@gmail.com>
2026-07-11 17:19:22 +0530

    changed middleware and baseurl

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,8 @@ app = FastAPI(root_path="/api/v1")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://trading-backtesting-application.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
